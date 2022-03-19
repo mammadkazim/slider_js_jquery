70c2a6602a473e4cf8ebd72f7c8c3521d3d4af8c
Kazim Mammadov <mammadkazim@gmail.com>
2022-03-20 02:08:42 +0400

    improved

--- js/app.js
+++ js/app.js
@@ -14,33 +14,66 @@ const images = [
     {
         name: 'tree',
         url: 'tree.jpg'
-    }
+    },
+
 ]
+
+const dots = document.getElementById('dots-container')
+
+for(let i = 0; i < images.length; i++){
+    let dot = document.createElement('div')
+    dot.classList.add(`dot`,i)
+    dots.append(dot)
+    dot.addEventListener('click',(e)=>{
+        img.setAttribute('src',images[i].url)
+        let active = dots.querySelector('.active')
+        if(active) active.classList.remove('active')
+        e.target.classList.add('active')
+    })
+}
+
+const dotsCollection = dots.getElementsByTagName('div')
+dotsCollection[0].classList.add('active')
+
+function activeClass (){
+    let active = dots.querySelector('.active')
+    if(active) active.classList.remove('active')
+    dotsCollection[count].classList.add('active')
+}
+
+function slideRight (){
+    count++
+    count = count === images.length ? 0 : count
+    img.setAttribute('src',images[count].url)
+}
+
+function slideLeft (){
+    count--
+    count = count < 0 ? images.length-1 : count
+    img.setAttribute('src',images[count].url)
+}
+
 let count = 0
 const img = document.querySelector('img')
 const rightBtn = document.querySelector('.right-btn')
 const leftBtn = document.querySelector('.left-btn')
 rightBtn.addEventListener('click',()=>{
-    count++
-    count = count === 4 ? 0 : count
-    img.setAttribute('src',images[count].url)
+    slideRight ()
+    activeClass ()
 })
 leftBtn.addEventListener('click',()=>{
-    count--
-    count = count < 0 ? 3 : count
-    img.setAttribute('src',images[count].url)
+    slideLeft ()
+    activeClass ()
 })
 document.addEventListener('keydown',(e)=>{
     if(e.keyCode === 39){
-    count++
-    count = count === 4 ? 0 : count
-    img.setAttribute('src',images[count].url)
+    slideRight ()
+    activeClass ()
 }
 })
 document.addEventListener('keydown',(e)=>{
     if(e.which === 37){
-        count--
-        count = count < 0 ? 3 : count
-        img.setAttribute('src',images[count].url)
+        slideLeft ()
+        activeClass ()
 }
 })
